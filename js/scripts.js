eafbe24c4f82a03bdce1bca73c5f7498ad6a4d81
bimik <bimik@o2.pl>
2017-05-27 11:40:19 +0200

    1.0.2

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@
 
 var a = prompt('Podaj a');
 var h = prompt('Podaj h');
-var triangleArea = a*h/2
-alert ('triangleArea')
+var triangleArea = a*h/2;
+alert(triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
