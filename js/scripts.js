95b9fd0c1d39e87035d335ee66655d66ea1cd47f
bimik <bimik@o2.pl>
2017-05-27 11:33:06 +0200

    Pole

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+var a = prompt('Podaj a')
+var h = prompt('Podaj h')
+var triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
